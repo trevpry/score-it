07ee17969021a166a7cc1658b8e0023311b82fad
TrevPry <cuoredoro14@gmail.com>
2020-06-06 20:28:17 -0700

    Updates

---
+++ precache-manifest.8cf024dbf55898275ec3ac663b6bfb90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2b205f767bd5dcc00d6b88eb56d778",
+    "url": "/score-it/index.html"
+  },
+  {
+    "revision": "5d5e8f62025b535ed7f6",
+    "url": "/score-it/static/css/main.ab5a5666.chunk.css"
+  },
+  {
+    "revision": "cacfa0c85f1303de4549",
+    "url": "/score-it/static/js/2.902e72f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/score-it/static/js/2.902e72f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5e8f62025b535ed7f6",
+    "url": "/score-it/static/js/main.4bab2745.chunk.js"
+  },
+  {
+    "revision": "9389b16e8fd291f6be4b",
+    "url": "/score-it/static/js/runtime-main.eac76c13.js"
+  }
+]);
